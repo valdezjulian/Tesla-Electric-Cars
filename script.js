bcc79edfa87e8a63d128ffdf06aa907a65f1e2dc
valdezjulian <julian.valdez531@gmail.com>
2024-07-24 07:16:25 -0400

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector(".formulario");
+
+function aparecerFormulario() {
+    formulario.style.display = "block";
+}
+
+function desaparecerFormulario() {
+    formulario.style.display = "none";
+}
+
+
